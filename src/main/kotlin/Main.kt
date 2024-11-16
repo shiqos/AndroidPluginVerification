0fa476ff1962039b5f714108e256af06b12b84db
shiq <shiqcode@gmail.com>
2024-11-16 16:15:48 +0800

    Add New Version

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -5,9 +5,14 @@ fun main() {
     Log.setLogLevel(Log.INFO)
 
 //    verify("241.17011.79", "https://plugins.jetbrains.com/plugin/download?rel=true&updateId=544295")
-    verify("242.23339.11", "https://plugins.jetbrains.com/plugin/download?rel=true&updateId=608438")
+
+//    verify("242.23339.11", "https://plugins.jetbrains.com/plugin/download?rel=true&updateId=608438")
 //    verify("242.21829.142", "https://plugins.jetbrains.com/plugin/download?rel=true&updateId=595106")
+//    verify("242.23726.38", "https://plugins.jetbrains.com/plugin/download?rel=true&updateId=619530")
+//    verify("242.23726.103", "https://plugins.jetbrains.com/plugin/download?rel=true&updateId=622869")
+
 //    verify("243.15521.24", "https://plugins.jetbrains.com/plugin/download?rel=true&updateId=605440")
+    verify("243.21565.214", "https://plugins.jetbrains.com/plugin/download?rel=true&updateId=634449")
 }
 
 private fun verify(pluginVersion: String, pluginDownloadUrl: String) {
